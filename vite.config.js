dcb0f26bfa8dd1de3554e587a7abcfc16005c219
Mark Malstrom <mark@malstrom.me>
2023-09-07 11:27:53 -0500

    Externalize media files in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    publicDir: "public"
+    build: {
+        rollupOptions: {
+            external: /^.*\.(mov|gif|png|svg|jpg)$/
+        }
+    }
 })
